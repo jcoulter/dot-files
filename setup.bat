7cdc52e5d54446f17f8bbf0f57791739d2aec18e
Jacob Coulter <jacob.coulter@gmail.com>
2025-03-10 09:00:33 -0400

    ignore .idea by default and link .asdfrc

--- setup.bat
+++ setup.bat
@@ -3,4 +3,5 @@ cp .bashrc ../.bashrc
 cp .zshrc ../.zshrc
 ln -s ~/dot-files/default-npm-packages ~/.default-npm-packages
 ln -s ~/dot-files/default-gems ~/.default-gems
+ln -s ~/dot-files/.asdfrc ~/.asdfrc
 
